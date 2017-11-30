20bcecea7a45e93031f0ffb04db235ff78d004ea
Charles Winston <charles.winston@tufts.edu>
2017-11-30 10:18:06 -0500

    requirements and more pyspark tests

---
+++ change_data.py
@@ -0,0 +1,11 @@
+# change_data.py
+import json
+fin = open('tweets1.txt', 'r')
+fout = open('tweets_new.txt', 'w')
+tweets_input = json.loads(fin.read())
+for label in tweets_input:
+    # ignore independents for now
+    if label != 'I':
+        for user in tweets_input[label]:
+            for tweet in tweets_input[label][user]:
+                fout.write(label + ' ' + tweet.replace('\n', ' ') + '\n')
--- pysparktest.py
+++ pysparktest.py
@@ -2,7 +2,7 @@
 import pyspark
 
 sc = pyspark.SparkContext()
-rdd = sc.parallelize(['Hello,', 'world!'])
-words = sorted(rdd.collect())
+rdd = sc.textfile('tweets_new.txt')
+result = rdd.map(lambda s: len(s)).reduce(lambda a, b: a + b)
 
-print words
+print result
