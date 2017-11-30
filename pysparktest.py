5c2f9dd14b52e7d93a83b762bbf498c4a6f28c22
Charles Winston <charles.winston@tufts.edu>
2017-11-30 10:20:06 -0500

    tiny

--- pysparktest.py
+++ pysparktest.py
@@ -2,7 +2,7 @@
 import pyspark
 
 sc = pyspark.SparkContext()
-rdd = sc.textfile('tweets_new.txt')
+rdd = sc.textFile('tweets_new.txt')
 result = rdd.map(lambda s: len(s)).reduce(lambda a, b: a + b)
 
 print result
